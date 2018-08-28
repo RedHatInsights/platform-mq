cd95a5e1099811bd40da044170ae49d3781eae50
Chris Mitchell <cmitchel@redhat.com>
2018-08-28 18:20:41 -0400

    Updates and cleanup

--- topics/create_topics.py
+++ topics/create_topics.py
@@ -1,14 +1,21 @@
 import jinja2
 import json
 import subprocess
+import argparse
 from tempfile import NamedTemporaryFile
+
+
+DESCRIPTION = 'Create required testing topics on a Strimzi Kafka cluster'
+
+parser = argparse.ArgumentParser(description=DESCRIPTION)
+parser.add_argument('namespace', help='OpenShift namespace in which to create the topics.')
+args = parser.parse_args()
 
 CONFIG = {
     "PARTITIONS": 1,
     "REPLICAS": 3,
     "KAFKA_CLUSTER": 'platform-mq'
 }
-NAMESPACE = 'cmitchel-testing'
 
 templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
@@ -23,4 +30,4 @@ for topic in topics:
     with NamedTemporaryFile(dir='.') as f:
         f.write(template.render(**CONFIG))
         f.flush()
-        subprocess.call(['oc', 'create', '-n', NAMESPACE, '-f', f.name])
+        subprocess.call(['oc', 'create', '-n', args.namespace, '-f', f.name])
