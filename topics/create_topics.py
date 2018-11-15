1579d1019f2de174c0b152e4340ca484d90f184b
Chris Mitchell <cmitchel@redhat.com>
2018-11-15 16:20:18 -0500

    Fix up topic creation and move topic configs external to creation script

--- topics/create_topics.py
+++ topics/create_topics.py
@@ -5,18 +5,18 @@ import argparse
 from tempfile import NamedTemporaryFile
 
 
-DESCRIPTION = 'Create required testing topics on a Strimzi Kafka cluster'
-TEMPLATE_FILE = "topictemplate.yaml"
-TOPIC_LIST = "./topics.json"
+DESCRIPTION = 'Create expected topics on a Strimzi Kafka cluster'
+TEMPLATE_FILE = "./topictemplate.yaml"
+TOPIC_JSON = "./topics.json"
 
 parser = argparse.ArgumentParser(description=DESCRIPTION)
 parser.add_argument('namespace', help='OpenShift namespace in which to create the topics.')
-parser.add_argument('-c', '--cluster', help='Strimzi cluster in which to create the topics.')
+parser.add_argument('cluster', help='Strimzi cluster in which to create the topics.')
+parser.add_argument('-d', '--dryrun', action='store_true',
+                    help='Print topics to be created without applying them')
 args = parser.parse_args()
 
 CONFIG = {
-    "PARTITIONS": 3,
-    "REPLICAS": 3,
     "KAFKA_CLUSTER": args.cluster,
     "NAMESPACE": args.namespace
 }
@@ -25,12 +25,18 @@ templateLoader = jinja2.FileSystemLoader(searchpath="./")
 templateEnv = jinja2.Environment(loader=templateLoader)
 template = templateEnv.get_template(TEMPLATE_FILE)
 
-with open(TOPIC_LIST) as f:
+with open(TOPIC_JSON) as f:
     topics = json.load(f)
 
 for topic in topics:
-    CONFIG["TOPIC_NAME"] = topic
-    with NamedTemporaryFile(dir='.') as tmpl:
-        tmpl.write(template.render(**CONFIG))
-        tmpl.flush()
-        subprocess.call(['oc', 'create', '-n', args.namespace, '-f', tmpl.name])
+    if 'TOPIC_CONFIGS' not in topic:
+        topic['TOPIC_CONFIGS'] = "{}"
+    topic.update(CONFIG)
+    rendered = template.render(**topic)
+    if args.dryrun:
+        print(rendered)
+    else:
+        with NamedTemporaryFile(dir='.') as tmpl:
+            tmpl.write(template.render(**topic))
+            tmpl.flush()
+            subprocess.call(['oc', 'apply', '-n', args.namespace, '-f', tmpl.name])
