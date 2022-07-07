e1b0ac766cc16b042915df61391dd22af4e3a6ce
Chris Mitchell <cmitchel@redhat.com>
2022-07-07 11:44:10 -0400

    Update test consumer to use plainauth
    Signed-off-by: Chris Mitchell <cmitchel@redhat.com>

--- mk_testing_consumer.py
+++ mk_testing_consumer.py
@@ -4,23 +4,6 @@ from oauthlib.oauth2 import BackendApplicationClient
 import ssl
 import argparse
 
-token_url = "https://identity.api.openshift.com/auth/realms/rhoas/protocol/openid-connect/token"
-
-
-class TokenProvider(object):
-    def __init__(self, client_id, client_secret, token_url):
-        self.client_id = client_id
-        self.client_secret = client_secret
-        self.token_url = token_url
-
-    def token(self):
-        client = BackendApplicationClient(client_id=self.client_id)
-        oauth = OAuth2Session(client=client)
-        token_json = oauth.fetch_token(token_url=token_url, client_id=self.client_id, client_secret=self.client_secret)
-        token = token_json['access_token']
-        return token
-
-
 parser = argparse.ArgumentParser()
 reqd = parser.add_argument_group('required arguments')
 reqd.add_argument('-b','--bootstrap_servers', nargs='+', required=True)
@@ -34,8 +17,9 @@ consumer = KafkaConsumer(
     group_id="mk_test_consumer",
     bootstrap_servers=args.bootstrap_servers,
     security_protocol='SASL_SSL',
-    sasl_mechanism='OAUTHBEARER',
-    sasl_oauth_token_provider=TokenProvider(args.client_id, args.client_secret, token_url),
+    sasl_mechanism='PLAIN',
+    sasl_plain_username = args.client_id,
+    sasl_plain_password = args.client_secret,
     ssl_check_hostname=False,
     auto_offset_reset='earliest',
     ssl_context=ssl.create_default_context(),
